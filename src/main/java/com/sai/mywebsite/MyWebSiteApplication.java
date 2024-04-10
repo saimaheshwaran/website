271a20552a9213ab2f671e08bd7018dc9a9cbdac
saimaheshwaran <saimaheshwaran@outlook.com>
2024-04-10 15:57:30 -0400

    Initial commit

---
+++ src/main/java/com/sai/mywebsite/MyWebSiteApplication.java
@@ -0,0 +1,13 @@
+package com.sai.mywebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWebSiteApplication.class, args);
+    }
+
+}
